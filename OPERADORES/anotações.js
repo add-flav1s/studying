e6bae7e9d3c6d86ec6b8cda775ccea373d8038ad
add-flav1s <aflavia17_@hotmail.com>
2021-01-11 18:25:26 -0300

    Estudando sobre os operadores em js

---
+++ OPERADORES/anotações.js
@@ -0,0 +1,17 @@
+//Operadores incremento e decremento
+//idade = 23
+//console.log(idade++);   //(++idade);
+//console.log(idade--);   //(--idade);
+
+//Operadores atribuição
+//valorCarro = 3.000
+//valorCarro +=valorCarro (mesma coisa de: valorCarro = valorCarro + valorCarro)
+
+//Operadores de igualdade
+//igualdade estrita
+//console.log(1 === 1); isso dá true (compara o valor e o tipo)
+//console.log('1' === 1); isso dá false (compara o valor e o tipo)
+
+//igualdade solta
+//console.log(1 == 1); isso dá true (compara apenas o valor)
+//console.log('1' == 1); isso dá true (compara apenas o valor)
