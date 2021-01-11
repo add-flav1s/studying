b68b6d72677f8ef80152ad0c2546922fbf2f852b
add-flav1s <aflavia17_@hotmail.com>
2021-01-11 18:24:24 -0300

    Estudando sobre functions

---
+++ FUNCTIONS/js/index.js
@@ -0,0 +1,25 @@
+// FUNCTION 1 //
+
+function calculaDolar(valor) {
+ resultado = valor / 5.19;
+}
+
+calculaDolar(30);
+console.log(resultado);
+
+// FUNCTION 2 //
+
+function multiplicaDois(num){
+  return num * 2;
+}
+console.log(multiplicaDois(4));
+//mesma coisa que escrever//
+//resultado = multiplicaDois(4);
+//console.log(resultado)
+
+// FUNCTION 3 //
+
+function digaNome(){
+  console.log("Souza");
+}
+digaNome();
